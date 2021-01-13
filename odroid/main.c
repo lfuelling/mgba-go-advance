d558297ff78407c616e9d6d10ce994c288cb03a8
Lukas Fülling <lukas@k40s.net>
2021-01-13 20:21:00 +0100

    move cheats before core init

--- odroid/main.c
+++ odroid/main.c
@@ -377,6 +377,32 @@ int main(int argc, char** argv)
 		return false;
 	}
 
+    // Cheats
+    char* cheatFileName = (char*)malloc(strlen(fileName) + 4 + 1);
+    strcpy(cheatFileName, fileName);
+    strcat(cheatFileName, ".cheats");
+
+    char* cheatFilePath = PathCombine(homedir, cheatFileName);
+    printf("cheatFilePath='%s'\n", cheatFilePath);
+
+    struct mCheatDevice* device = NULL;
+    bool success = true;
+    struct VFile* vf = VFileOpen(cheatFilePath, O_RDONLY);
+    if (vf && (device = core->cheatDevice(core))) {
+        printf("Parsing cheats...\n");
+        mCheatDeviceClear(device);
+        success = mCheatParseFile(device, vf);
+        vf->close(vf);
+    } else {
+        success = false;
+    }
+
+    if(!success) {
+        printf("Error parsing cheats!\n");
+    } else {
+        printf("Cheats loaded...\n");
+    }
+
 	// Initialize the received core.
     mCoreInitConfig(core, NULL);
 	core->init(core);
@@ -587,32 +613,6 @@ int main(int argc, char** argv)
 
     char* savePath = PathCombine(homedir, saveName);
     printf("savePath='%s'\n", savePath);
-
-    // Cheats
-    char* cheatFileName = (char*)malloc(strlen(fileName) + 4 + 1);
-    strcpy(cheatFileName, fileName);
-    strcat(cheatFileName, ".cheats");
-
-    char* cheatFilePath = PathCombine(homedir, cheatFileName);
-    printf("cheatFilePath='%s'\n", cheatFilePath);
-
-    struct mCheatDevice* device = NULL;
-    bool success = true;
-    struct VFile* vf = VFileOpen(cheatFilePath, O_RDONLY);
-    if (vf && (device = core->cheatDevice(core))) {
-        printf("Parsing cheats...\n");
-        mCheatDeviceClear(device);
-        success = mCheatParseFile(device, vf);
-        vf->close(vf);
-    } else {
-        success = false;
-    }
-
-    if(!success) {
-        printf("Error parsing cheats!\n");
-    } else {
-        printf("Cheats loaded...\n");
-    }
 
     // SRAM
     char* sramName = (char*)malloc(strlen(fileName) + 4 + 1);
